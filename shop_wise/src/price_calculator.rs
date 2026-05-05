d758e48b3f5e8c90b6ca462aea6eea1da142ba74
worthalex@myvuw.ac.nz <worthalexander@protonmail.com>
2026-05-05 20:09:45 +1200

    Added initial project structure

---
+++ shop_wise/src/price_calculator.rs
@@ -0,0 +1,37 @@
+pub mod shopping_list_parser;
+
+pub mod item_resolver;
+
+pub mod calculator {
+    use crate::{ShoppingItem, Supermarket};
+
+    pub fn calculate(
+        items: &[Option<ShoppingItem>],
+        valid_supermarkets: &[Supermarket],
+        possible_routes: &[Route],
+        /* database: idfk what type this should be */
+    ) {
+    }
+}
+
+pub struct ShoppingItem<'a> {
+    name: &'a str,
+}
+
+// Maybe this belongs in Database?
+pub struct Supermarket {/* id: ??? */}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
