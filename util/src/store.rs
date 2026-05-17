c891234015b5f4111bc5a7e165c08942dfae235d
worthalex@myvuw.ac.nz <worthalexander@protonmail.com>
2026-05-18 11:20:54 +1200

    Added serialisation and deserialisation

--- util/src/store.rs
+++ util/src/store.rs
@@ -1,15 +1,18 @@
+use serde::{Deserialize, Serialize};
 use strum_macros::EnumIter;
 
 use crate::coordinate::Coordinate;
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, EnumIter)]
+#[derive(
+    Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone, Copy, EnumIter, Serialize, Deserialize,
+)]
 pub enum StoreBrand {
     Paknsave,
     Newworld,
     Woolworths,
 }
 
-#[derive(Debug, PartialEq, PartialOrd, Clone, Copy)]
+#[derive(Debug, PartialEq, PartialOrd, Clone, Copy, Serialize, Deserialize)]
 pub struct Store {
     pub brand: StoreBrand,
     pub location: Coordinate,
