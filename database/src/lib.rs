485e2c0bdbc7318832cdb2277280203f4336d0d9
Samuel Smith <smithsamu11@ecs.vuw.ac.nz>
2026-07-17 14:03:24 +1200

    Revert "update branch to test pipeline"
    This reverts commit d12a7d3f19ec78e35c84cde810377024e56b5673

--- database/src/lib.rs
+++ database/src/lib.rs
@@ -95,7 +95,7 @@ impl Database {
         );
 
         // Combine the chain names and the LIKE pattern into one
-        // parameter iterator. Extra WORD
+        // parameter iterator.
         let pattern = format!("%{item_query}%");
         let mut params: Vec<&dyn rusqlite::ToSql> = in_range_chains
             .iter()
