3238413dda93292e1367a9d19d5253045631e0aa
Samuel Smith <smithsamu11@ecs.vuw.ac.nz>
2026-09-02 00:37:55 +1200

    Bring others up to date with ItemInfo removal

--- shop_wise/src/results.rs
+++ shop_wise/src/results.rs
@@ -156,9 +156,8 @@ impl Scenario {
                  let items = infos.items
                      .iter()
                     .map(|info| ItemLine {
-                        name: info.product_name.clone(),
-                         // TODO: ItemInfo::quantity is string
-                         quantity: 1,
+                        name: info.name.clone(),
+                         quantity: info.quantity,
                          unit_price: info.price.clone(),
                          // TODO: loyalty pricing
                          on_special: false,
