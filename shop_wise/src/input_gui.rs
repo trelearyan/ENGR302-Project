61c9fb1f131262587f579478f96f1453bf7aea48
Samuel Smith <smithsamu11@ecs.vuw.ac.nz>
2026-09-01 22:04:58 +1200

    Update input_gui to pass price_calculator the neccesary info for route_planner

--- shop_wise/src/input_gui.rs
+++ shop_wise/src/input_gui.rs
@@ -417,7 +417,7 @@ impl MyApp {
                     banned_stores.insert(StoreBrand::Woolworths);
                 }
 
-                let mut filters = StoreFilters::builder()
+                let mut filters_builder = StoreFilters::builder()
                     .location(
                         <RefCell<LocationState> as Clone>::clone(&self.location_state)
                             .into_inner()
@@ -428,8 +428,15 @@ impl MyApp {
                     .max_store_visits(self.filters.max_stores as usize)
                     .disallow_brands(banned_stores.iter().copied().collect::<Vec<_>>().deref());
                 let routes = route_planner::all_possible_routes(&filters.build());
+                let filters = filters_builder.build();
+                let routes = route_planner::all_possible_routes(&filters);
+
+                // Sam
+                let res = price_calculator::calculator::calculate(&self.shopping_items, &filters);
+                log::info!("{:?}", res);
+
                 let origin_label = self.location_state.borrow().address.clone();
-                self.results = match price_calculator::calculator::calculate(&self.shopping_items) {
+                self.results = match res {
                     Some(calc) => ResultsState::Ready(Box::new(
                         crate::results::Results::from_calculation(&calc, origin_label),
                     )),
