d98b3bfaab6ce5112f1a09c4cf3de7585a4e450a
Samuel Smith <smithsamu11@ecs.vuw.ac.nz>
2026-09-11 00:03:46 +1200

    Implement Search

--- shop_wise/src/price_calculator/item_resolver.rs
+++ shop_wise/src/price_calculator/item_resolver.rs
@@ -18,6 +18,14 @@ pub struct SantizedSearchResult {
     image_url: String,
 }
 
+
+#[derive(Debug, PartialEq)]
+pub struct SantizedSearchResult {
+    name: String,
+    unit: SearchUnits,
+    image_url: String,
+}
+
 #[derive(Debug, PartialEq)]
 struct SearchResult {
     item_id: u32,
