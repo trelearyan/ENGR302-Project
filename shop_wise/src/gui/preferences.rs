c13a4fa97d8551c64425874b844c0ddae727fc53
ry <trelearyan@myvuw.ac.nz>
2026-09-11 13:42:40 +1200

    preferences.rs hover text

--- shop_wise/src/gui/preferences.rs
+++ shop_wise/src/gui/preferences.rs
@@ -14,13 +14,17 @@ impl ShowableWidget for PreferencesData {
     fn show(&mut self, ui: &mut Ui) {
         ui.heading("Preferences");
 
-        ui.label("Max Range");
+        ui.label("Max Range")
+            .on_hover_text("How far would you want to travel from your location");
         let mut maxrange = self.max_range.kilometres();
-        ui.add(egui::Slider::new(&mut maxrange, 1.0..=50.).text("(km)"));
+        ui.add(egui::Slider::new(&mut maxrange, 1.0..=50.).text("(km)"))
+            .on_hover_text("Use the slider to pick up your range");
         self.max_range = Distance::from_kilometres_f64(maxrange);
 
-        ui.label("Max Stores per Trip");
-        ui.add(egui::Slider::new(&mut self.max_stores, 1..=10));
+        ui.label("Max Stores per Trip")
+            .on_hover_text("Select the amount of stores to place your range");
+        ui.add(egui::Slider::new(&mut self.max_stores, 1..=10))
+            .on_hover_text("Move the slider to select your maximum stores");
     }
 }
 
