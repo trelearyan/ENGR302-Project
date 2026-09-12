63cecf9754bf7a5f86f8b6793faaa323a3c70eb3
ry <trelearyan@myvuw.ac.nz>
2026-09-12 15:37:43 +1200

    added store location drop down

--- shop_wise/src/gui/supermarkets.rs
+++ shop_wise/src/gui/supermarkets.rs
@@ -1,52 +1,97 @@
-use eframe::egui::Ui;
+use eframe::egui::{self, Ui};
 use serde::{Deserialize, Serialize};
+use util::store::StoreBrand;
 
 use crate::gui::ShowableWidget;
+use crate::price_calculator::results::brand_label;
+
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct StorePreference {
+    pub store_id: u32,
+    pub name: String,
+    pub checked: bool,
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct ChainPreference {
+    pub brand: StoreBrand,
+    pub checked: bool,
+    pub has_loyalty_card: bool,
+    pub locations: Vec<StorePreference>,
+}
+
+impl ChainPreference {
+    fn new(brand: StoreBrand) -> Self {
+        Self {
+            brand,
+            checked: true,
+            has_loyalty_card: false,
+            locations: Vec::new(),
+        }
+    }
+}
+
+fn loyalty_label(brand: StoreBrand) -> &'static str {
+    match brand {
+        StoreBrand::Paknsave | StoreBrand::Newworld => "I have a Clubcard",
+        StoreBrand::Woolworths => "I have an Everyday Rewards Card",
+    }
+}
 
 #[derive(Clone, Serialize, Deserialize, Debug)]
 pub struct SupermarketsData {
-    pub include_paknsave: bool,
-    pub include_newworld: bool,
-    pub include_woolworths: bool,
-    pub paknsave_loyalty: bool,
-    pub newworld_loyalty: bool,
-    pub woolworths_loyalty: bool,
+    pub chains: Vec<ChainPreference>,
+}
+
+impl SupermarketsData {
+    #[must_use]
+    pub fn has_loyalty_card(&self, brand: StoreBrand) -> bool {
+        self.chains
+            .iter()
+            .find(|chain| chain.brand == brand)
+            .is_some_and(|chain| chain.has_loyalty_card)
+    }
 }
 
 impl ShowableWidget for SupermarketsData {
     fn show(&mut self, ui: &mut Ui) {
         ui.heading("Supermarkets")
-            .on_hover_text("Select the chains you wish to be included in your search");
+            .on_hover_text("Select the Supermarket chains you want to be included in your search");
 
-        let chains = [
-            (
-                &mut self.include_paknsave,
-                &mut self.paknsave_loyalty,
-                "Pak'nSave",
-                "I have a Clubcard",
-            ),
-            (
-                &mut self.include_newworld,
-                &mut self.newworld_loyalty,
-                "New World",
-                "I have a Clubcard",
-            ),
-            (
-                &mut self.include_woolworths,
-                &mut self.woolworths_loyalty,
-                "Woolworths",
-                "I have an Everyday Rewards Card",
-            ),
-        ];
-        for (included, loyalty, name, loyalty_label) in chains {
-        ui.checkbox(included, name)
-            .on_hover_text(format!("Include {name} stores in the search"));
-        if *included {
+        for chain in &mut self.chains {
+            let name = brand_label(chain.brand);
+            let card = loyalty_label(chain.brand);
+
+            ui.checkbox(&mut chain.checked, name)
+                .on_hover_text(format!("Include {name} stores in the search"));
+
+            if !chain.checked {
+                continue;
+            }
+
             ui.indent(name, |ui| {
-                ui.checkbox(loyalty, loyalty_label)
-                    .on_hover_text("Do you have a Pak'nSave loyalty card");
+                ui.checkbox(&mut chain.has_loyalty_card, card)
+                    .on_hover_text(format!("Do you have {name} loyalty card"));
+
+                egui::CollapsingHeader::new("Choose locations")
+                    .id_salt(name)
+                    .show(ui, |ui| {
+                        if chain.locations.is_empty() {
+                            ui.label(
+                                egui::RichText::new("not available yet")
+                                    .small()
+                                    .weak(),
+                            );
+                            return;
+                        }
+
+                        for store in &mut chain.locations {
+                            ui.checkbox(&mut store.checked, &store.name)
+                                .on_hover_text("Untick if you do not want this supermarket chain included in search");
+                        }
+                    });
             });
-        }
         }
     }
 }
@@ -54,12 +99,11 @@ impl ShowableWidget for SupermarketsData {
 impl Default for SupermarketsData {
     fn default() -> Self {
         Self {
-            include_paknsave: true,
-            include_newworld: true,
-            include_woolworths: true,
-            paknsave_loyalty: false,
-            newworld_loyalty: false,
-            woolworths_loyalty: false,
+            chains: vec![
+                ChainPreference::new(StoreBrand::Paknsave),
+                ChainPreference::new(StoreBrand::Newworld),
+                ChainPreference::new(StoreBrand::Woolworths),
+            ],
         }
     }
 }
