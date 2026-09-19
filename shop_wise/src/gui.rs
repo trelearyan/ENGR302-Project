06832e6a3c39f31f33e43c41e9e7190ce8786578
ry <trelearyan@myvuw.ac.nz>
2026-09-20 02:53:44 +1200

    at least arsenal are losing

--- shop_wise/src/gui.rs
+++ shop_wise/src/gui.rs
@@ -82,6 +82,7 @@ impl ShowableWidget for AppData {
                         ui.separator();
                         self.search_button(ui);
                         self.output_routes.show(ui);
+                        ui.add_space(20000.0);
                     })
             });
     }
